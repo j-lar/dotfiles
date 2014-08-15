2ba8013de902bc42ed3ee362a9ad46a47b440437
Jon Larsen <github@jonathanlarsen.com>
2014-08-15 04:43:33 +0400

    Added zsh, git and bash dotfiles

---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+### Variables
+
+dir=~/dotfiles
+olddir=~/dotfiles_old
+files=".bashrc .screenrc .gitconfig .zshrc"
+
+###
+
+# creade dofiles_old
+echo "Creating $olddir for backup of existing dotfiles"
+mkdir -p $olddir
+echo "...done"
+
+# change to dotfiles dir
+echo "Changing to $dir directory"
+cd $dir
+echo "...done"
+
+# move existing dotfiles to backup dir
+for file in $files; do
+	echo "Moving existing dotfiles to $olddir"
+	mv ~/$file ~/dotfiles_old/
+	echo "Creating symlinks"
+	ln -s $dir/$file ~/$file
+done
